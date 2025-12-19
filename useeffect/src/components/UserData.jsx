27246cbc0bb9ee0c39025a67d88b819fe24142eb
poshiplatta <tabishachouhan001@gmail.com>
2025-12-19 21:33:15 +0530

    USerData

---
+++ useeffect/src/components/UserData.jsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+
+function UserData() {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users/1")
+      .then((response) => response.json())
+      .then((data) => {
+        setUser(data);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <h2>Loading...</h2>;
+  }
+
+  return (
+    <div>
+      <h2>User Data</h2>
+      <p><b>Name:</b> {user.name}</p>
+      <p><b>Email:</b> {user.email}</p>
+      <p><b>Phone:</b> {user.phone}</p>
+    </div>
+  );
+}
+
+export default UserData;
